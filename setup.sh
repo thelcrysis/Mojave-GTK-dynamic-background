8a4a7d7f0566467621d2f68b27a0bc61c6d5a789
thelcrysis <38574790+thelcrysis@users.noreply.github.com>
2019-04-07 04:40:39 +0200

    added setup
    file to set up everything

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install unzip
+sudo apt-get install gsettings
+
+mkdir $HOME/Pictures/Wallpapers/mojave-dynamic
+wget https://files.rb.gd/mojave_dynamic.zip -P $HOME/Pictures/Wallpapers/mojave-dynamic/
+wget https://raw.githubusercontent.com/thelcrysis/Mojave-GTK-dynamic-background/master/mojave.xml -P $HOME/Pictures/Wallpapers/mojave-dynamic/ 
+unzip $HOME/Pictures/Wallpapers/mojave-dynamic/mojave_dynamic.zip -d $HOME/Pictures/Wallpapers/mojave-dynamic/
+rm -rf $HOME/Pictures/Wallpapers/mojave-dynamic/mojave_dynamic.zip 
+rm -rf $HOME/Pictures/Wallpapers/mojave-dynamic/__MACOSX
+sed -i -e "s/username/$USER/g" $HOME/Pictures/Wallpapers/mojave-dynamic/mojave.xml
+gsettings set org.gnome.desktop.background picture-uri "file:$HOME/Pictures/Wallpapers/mojave-dynamic/mojave.xml"
+
+
+
+
+
